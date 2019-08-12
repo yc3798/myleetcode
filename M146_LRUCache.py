a4d45caf7eb5827ecfd4fe74ee2cd83b0ac5a057
yc3798 <yc3798@nyu.edu>
2019-08-12 16:30:41 -0400

    m146

--- M146_LRUCache.py
+++ M146_LRUCache.py
@@ -1,3 +1,35 @@
+from collections import OrderedDict
+
+
+# Approach 2: ordered dict for O(1)
+#
+class LRUCache2(OrderedDict):
+    def __init__(self, capacity):
+        self.capacity = capacity
+
+    def get(self, key):
+        if key not in self:
+            return -1
+
+        self.move_to_end(key)
+        print(self)
+        return self[key]
+
+    def put(self, key, value):
+        if key in self:
+            self.move_to_end(key)
+        self[key] = value
+
+        if len(self) > self.capacity:
+            self.popitem(last=False)
+        print(self)
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+
 class LRUCache:
     # Approach 1: use a list to record "least used"
     #
@@ -36,10 +68,13 @@ class LRUCache:
             self.usage.append(key)
 
 
+if __name__ == '__main__':
+    mycache = LRUCache2(3)
+    mycache.put(5,1)
+    mycache.put(3,2)
+    mycache.put(5,2)
+    mycache.get(5)
+    mycache.put(4,0)
+    mycache.put(7,3)
+    # print(mycache)
 
-
-
-            # Your LRUCache object will be instantiated and called as such:
-            # obj = LRUCache(capacity)
-            # param_1 = obj.get(key)
-            # obj.put(key,value)
