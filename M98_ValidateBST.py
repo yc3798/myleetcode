6c6801e4b1eba2cfa4b2187fd66e2bf2aeb64697
yc3798 <yc3798@nyu.edu>
2019-10-16 13:56:25 -0400

    M98 improve

--- M98_ValidateBST.py
+++ M98_ValidateBST.py
@@ -1,9 +1,10 @@
-# Definition for a binary tree node.
+# T(n) =  
+import sys
 class TreeNode(object):
-    def __init__(self, x):
+    def __init__(self, x, l=None, r=None):
         self.val = x
-        self.left = None
-        self.right = None
+        self.left = l
+        self.right = r
 
 class Solution(object):
     def findmin(self,v):
@@ -27,7 +28,10 @@ class Solution(object):
             return max(self.findmax(v.left),v.val)
         else:
             return max(self.findmax(v.left),self.findmax(v.right),v.val)
-        
+	
+	#==================
+    # Naive approach  
+    # Runtime : n^2 
     def isValidBST(self, root):
         """
         :type root: TreeNode
@@ -43,3 +47,60 @@ class Solution(object):
             return False
         return True
     
+	#==================
+    # Linear, with rec helper 
+    def isValidBSTLinear(self,root):
+        return self.isBST(root, -sys.maxsize, sys.maxsize)
+
+    def isBST(self, root, mi, ma):
+    	if root is None:
+    		return True 
+    	if root.val < mi or root.val > ma:
+    		return False
+    	return self.isBST(root.left, mi, root.val) and self.isBST(root.right, root.val, ma)
+
+
+	#==================
+    # Linear, without rec helper
+    def isValidBSTLinear2(self,root):
+        """
+        Assume base case is leaf, not None 
+        :type root: TreeNode
+        :rtype: min, max, bool
+        """	
+        if root is None:
+        	return None, None, True 
+        if root.left is None and root.right is None:
+        	return root.val, root.val, True
+        	
+        lmin = sys.maxsize
+        rmin = sys.maxsize
+        lmax = -sys.maxsize
+        rmax = -sys.maxsize
+
+        if root.left is not None:
+       		lmin, lmax, lbool = self.isValidBSTLinear2(root.left)
+       		if root.val < lmax:
+       			lbool = False
+       		lmin = min(root.val, lmin)
+       		lmax = max(root.val, lmax)
+       		# print(root.val, lmin, lmax,lbool)
+       	if root.right is not None:
+       		rmin, rmax, rbool = self.isValidBSTLinear2(root.right)
+       		if root.val > rmin:
+       			lbool = False
+       		rmin = min(root.val, rmin)
+       		rmax = max(root.val, rmax)
+       		# print(root.val,rmin,rmax,rbool)       
+        return min(lmin,rmin), max(lmax,rmax), lbool and rbool
+
+
+s = Solution()
+
+t1 = TreeNode(5,TreeNode(1),TreeNode(6))
+t2 = TreeNode(5,TreeNode(3,TreeNode(2),TreeNode(4)),TreeNode(6))
+t3 = TreeNode(5,TreeNode(3,TreeNode(2),TreeNode(6)),TreeNode(6))
+testlist = [t1,t2,t3]
+for t in testlist:
+	print(s.isValidBSTLinear2(t))
+
